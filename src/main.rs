465f3cb7efbba453c640a2bb0c15ed1e8319164e
Pepi <pepimartinez@protonmail.com>
2025-02-20 04:14:02 -0400

    logs

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,9 @@ static TOTAL_ATTEMPTS: AtomicU64 = AtomicU64::new(0);
 
 #[tokio::main]
 async fn main() {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
     env_logger::init();
 
     let num_threads = num_cpus::get();
